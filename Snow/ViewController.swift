ddce7055cde40f29d8e40d4bbc6543667a33144f
rainbowshan33 <rainbowshan33@gmail.com>
2021-04-17 21:52:48 +0800

    加入gif及音樂，完

--- Snow/ViewController.swift
+++ Snow/ViewController.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 import SpriteKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    //音樂
+    let player = AVPlayer()
+
     override func viewDidLoad() {
          super.viewDidLoad()
         
@@ -26,15 +30,15 @@ class ViewController: UIViewController {
         let snowEmitterCell = CAEmitterCell()
         snowEmitterCell.contents = UIImage(named: "snowflake")?.cgImage
         //設定每秒發射幾個雪花
-        snowEmitterCell.birthRate = 2
+        snowEmitterCell.birthRate = 8
         //雪花維持的秒數
-        snowEmitterCell.lifetime = 20
+        snowEmitterCell.lifetime = 18
         //雪花移動的速度
-        snowEmitterCell.velocity = 50
+        snowEmitterCell.velocity = 55
         //雪花的大小
         snowEmitterCell.scale = 3
         //雪花往下落下的加速度
-        snowEmitterCell.yAcceleration = 5
+        snowEmitterCell.yAcceleration = 3
         //雪花發射的路徑
         
         let snowEmitterLayer = CAEmitterLayer()
@@ -47,20 +51,35 @@ class ViewController: UIViewController {
         snowEmitterCell.scale = 0.5
         snowEmitterCell.scaleRange = 0.3
         //雪花大小改變的速度
-        snowEmitterCell.scaleSpeed = -0.02
+        snowEmitterCell.scaleSpeed = -0.01
         //旋轉吧，雪花
         snowEmitterCell.spin = 0.5
         snowEmitterCell.spinRange = 1
         //雪花發射的角度範圍
         snowEmitterCell.emissionRange = CGFloat.pi
         
+        //產生 image view
+        let deerImageView = UIImageView(frame: CGRect(x: 85, y: 450, width: 300, height: 300))
+        view.addSubview(deerImageView)
         
+        //產生代表動畫圖片的 UIImage 物件
+        let animatedImage = UIImage.animatedImageNamed("20161229123734_wEav8-",duration: 1)
         
+        deerImageView.image = animatedImage
+    
+        //加入音樂
+        let fileUrl = Bundle.main.url(forResource: "bensound-tomorrow", withExtension:"mp3")!
+                let playerItem = AVPlayerItem(url: fileUrl)
+                player.replaceCurrentItem(with: playerItem)
+                player.play()
         
-        
-        
-     }
     }
+}
+        
+        
+        
+        
+    
         
         
 
